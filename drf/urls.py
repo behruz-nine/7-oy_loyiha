2e9d46814607bada1b70505570e6c3473ee15646
Behruz Quzibayev <bexruzquziboyev@gmail.com>
2026-08-17 12:55:42 +0500

    Yana yangi 4)put_product(PUT), 5)patch_product(PATCH), 6)delete_product(DELETE) nomli funksiyalar views.py faylida yaratildi

--- drf/urls.py
+++ drf/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [path('create/', views.create_product),
                path('get/', views.getproducts),
-               path('detail/<int:product_id>/', views.product_details)]
+               path('detail/<int:product_id>/', views.product_details),
+               path('put/<int:product_id>/', views.put_product),
+               path('patch/<int:product_id>/', views.patch_product),
+               path('delete/<int:product_id>/', views.delete_product)]
 
--- drf/views.py
+++ drf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Product
 
 
@@ -66,5 +67,73 @@ def product_details(request, product_id):
         { 'msg': 'Product topildi'}, status= status.HTTP_201_CREATED
     )
 
+#===================================================
+
+@api_view(['PUT'])
+def put_product(request, product_id):
+    product = Product.objects.filter(id = product_id).first()
+    if product:
+        product.product_name = request.data.get('product_name')
+        product.description = request.data.get('description')
+        product.price = request.data.get('price')
+        product.quantity = request.data.get('quantity')
+
+        product.save()
+
+    return Response(
+        {
+            'msg': 'Product updated successfully',
+            'status': status.HTTP_200_OK
+        }
+    )
+
+#===================================================
+
+@api_view(['PATCH'])
+def patch_product(request, product_id):
+    product = Product.objects.filter(id=product_id).first()
+    if not product:
+        raise NotFound('This product is not founded')
+    
+    product_name = request.data.get('product_name')
+    description = request.data.get('description')
+    price = request.data.get('price')
+    quantity = request.data.get('quantity')
+
+    if product_name:
+        product.product_name = product_name
+
+    if description:
+        product.description = description
+
+    if price:
+        product.price = price
+
+    if quantity:
+        product.quantity = quantity
+
+    product.save()
+
+    return Response(
+        {
+            'msg': 'Product is updated partially',
+            'status': status.HTTP_200_OK
+        }
+    )
+
+#=====================================================
+
+@api_view(['DELETE'])
+def delete_product(request, product_id):
+    product = Product.objects.filter(id = product_id).first()
+    product.delete()
+
+    return Response(
+        {
+            'msg': 'Product is deleted successfully',
+            'status': status.HTTP_200_OK
+        }
+    )
 
 
+    
